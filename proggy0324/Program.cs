44ccd19295ca888be457ecd162e570980da85592
biherczlorant <biherczlori@gmail.com>
2021-03-24 14:42:24 +0100

    v1

---
+++ proggy0324/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proggy0324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
